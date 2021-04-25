185a8fc8ddd20cc31d44087cd8270a9c987ec600
Gustavo Vasconcelos <gustavo.vas.bl@gmail.com>
2021-04-25 15:04:34 -0300

    :rocket: NLW 5 ended

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,10 +3,12 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { api } from '../../services/api';
 import Image from 'next/image'
+import Head from 'next/head'
 import Link from 'next/link'
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 import styles from './episode.module.scss'
+import { usePlayer } from '../../contexts/PlayerContext';
 
 type Episode = {
     id: string;
@@ -25,8 +27,13 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode } : EpisodeProps){
+    const { play } = usePlayer();
+
     return (
         <div className={styles.episode}>
+            <Head>
+                <title>{episode.title} | Podcastr</title>
+            </Head>
             <div className={styles.thumbnailContainer}>
                 <Link href="/">
                     <button type="button">
@@ -39,7 +46,7 @@ export default function Episode({ episode } : EpisodeProps){
                     src={episode.thumbnail}
                     objectFit="cover"
                 />
-                <button type="button"> 
+                <button type="button" onClick={() => {play(episode)}}> 
                     <img src="/play.svg" alt="Tocar episódio"/>
                 </button>
             </div>
@@ -58,8 +65,24 @@ export default function Episode({ episode } : EpisodeProps){
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get('episodes',{
+        params: {
+            _limit: 2,
+            _sort: "published_at",
+            _order: "desc"
+        }
+    })
+
+    const paths = data.map(episode =>{
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+
     return{
-        paths: [],
+        paths,
         fallback: "blocking"
     }
 }
